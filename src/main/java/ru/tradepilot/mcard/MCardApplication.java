55091b5ab700b87756059b92e75b3db5d19f46bc
Nail Zagidulin <karimnail96@gmail.com>
2018-02-25 23:36:03 +0500

    Initial commit

---
+++ src/main/java/ru/tradepilot/mcard/MCardApplication.java
@@ -0,0 +1,12 @@
+package ru.tradepilot.mcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MCardApplication.class, args);
+	}
+}
