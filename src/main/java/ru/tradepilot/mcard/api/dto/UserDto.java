e36cb8a669b5533d5d8988cf2fc541a906478232
Nail Zagidulin <karimnail96@gmail.com>
2018-03-02 21:04:16 +0500

    Validation DTO models

--- src/main/java/ru/tradepilot/mcard/api/dto/UserDto.java
+++ src/main/java/ru/tradepilot/mcard/api/dto/UserDto.java
@@ -1,16 +1,21 @@
 package ru.tradepilot.mcard.api.dto;
 
 
+import ru.tradepilot.mcard.validation.ValidEmail;
+import ru.tradepilot.mcard.validation.ValidPassword;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserDto {
+    @ValidEmail
     @NotNull
-    @Size(min = 1, message = "{size.userdto.email}")
+    @Size(min = 5, message = "{size.userdto.email}")
     private String email;
 
+    @ValidPassword
     @NotNull
-    @Size(min = 1)
+    @Size(min = 6)
     private String password;
 
     private String token;
